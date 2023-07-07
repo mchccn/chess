b165f6e579f4d08d543139653febef49465b1930
kelsny <cursors.owo.uwu@gmail.com>
2023-07-07 18:32:32 -0500

    Revert "Revert "fen util""
    This reverts commit 90136111f46be0e644cccc693f30ad88db6d030c.

---
+++ src/Board.ts
@@ -0,0 +1,6 @@
+export class Board {
+    readonly squares = new Array<number>(64).fill(0);
+
+    constructor () {
+    }
+}
---
+++ src/BoardRepresentation.ts
@@ -0,0 +1,4 @@
+export class BoardRepresentation {
+    static rankNames = "12345678";
+    static fileNames = "abcdefgh";
+}
