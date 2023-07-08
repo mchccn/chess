b010198866b17a6f7e468f345e04eb246bd5a840
kelsny <cursors.owo.uwu@gmail.com>
2023-07-08 11:56:53 -0500

    clean up app

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,43 +1,14 @@
 import { Move } from "../../dist/Move.js";
 import { Board, BoardRepresentation, MoveGenerator, Piece } from "../../dist/index.js";
+import { iconMap } from "./iconMap.js";
+import { setup } from "./setup.js";
 
-const iconMap = {
-    [Piece.None]: "none.svg",
-    [Piece.White | Piece.King]: "white_king.svg",
-    [Piece.White | Piece.Pawn]: "white_pawn.svg",
-    [Piece.White | Piece.Knight]: "white_knight.svg",
-    [Piece.White | Piece.Bishop]: "white_bishop.svg",
-    [Piece.White | Piece.Rook]: "white_rook.svg",
-    [Piece.White | Piece.Queen]: "white_queen.svg",
-    [Piece.Black | Piece.King]: "black_king.svg",
-    [Piece.Black | Piece.Pawn]: "black_pawn.svg",
-    [Piece.Black | Piece.Knight]: "black_knight.svg",
-    [Piece.Black | Piece.Bishop]: "black_bishop.svg",
-    [Piece.Black | Piece.Rook]: "black_rook.svg",
-    [Piece.Black | Piece.Queen]: "black_queen.svg",
-};
+setup();
 
 // starting position fen "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 // sliding pieces move generation test fen "8/8/4Q3/8/3B4/8/2R5/8 w KQkq - 0 1"
 
 const board = new Board().loadPosition("8/8/4Q3/8/3B4/8/pR2p3/8 w KQkq - 0 1");
-
-const filesElement = document.querySelector(".files")!;
-const ranksElement = document.querySelector(".ranks")!;
-
-for (const f of BoardRepresentation.fileNames) {
-    const fileDiv = document.createElement("div");
-    fileDiv.textContent = f;
-
-    filesElement.append(fileDiv);
-}
-
-for (const r of BoardRepresentation.rankNames) {
-    const rankDiv = document.createElement("div");
-    rankDiv.textContent = r;
-
-    ranksElement.append(rankDiv);
-}
 
 const boardElement = document.querySelector(".board")!;
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { Board } from "./Board.js";
 export { BoardRepresentation } from "./BoardRepresentation.js";
 export { FEN, FENInfo } from "./FEN.js";
+export { Move } from "./Move.js";
 export { MoveGenerator } from "./MoveGenerator.js";
 export { Piece } from "./Piece.js";
