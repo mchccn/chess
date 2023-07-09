c3ec48d4c0e6c30e6d0ea68e87bc6d9400d22f10
kelsny <cursors.owo.uwu@gmail.com>
2023-07-08 22:25:36 -0500

    align more stuff in code

--- app/src/iconMap.ts
+++ app/src/iconMap.ts
@@ -1,17 +1,17 @@
 import { Piece } from "../../dist/index.js";
 
 export const iconMap = {
-    [Piece.None]: "none.svg",
-    [Piece.White | Piece.King]: "white_king.svg",
-    [Piece.White | Piece.Pawn]: "white_pawn.svg",
+    [Piece.None                ]: "none.svg"        ,
+    [Piece.White | Piece.King  ]: "white_king.svg"  ,
+    [Piece.White | Piece.Pawn  ]: "white_pawn.svg"  ,
     [Piece.White | Piece.Knight]: "white_knight.svg",
     [Piece.White | Piece.Bishop]: "white_bishop.svg",
-    [Piece.White | Piece.Rook]: "white_rook.svg",
-    [Piece.White | Piece.Queen]: "white_queen.svg",
-    [Piece.Black | Piece.King]: "black_king.svg",
-    [Piece.Black | Piece.Pawn]: "black_pawn.svg",
+    [Piece.White | Piece.Rook  ]: "white_rook.svg"  ,
+    [Piece.White | Piece.Queen ]: "white_queen.svg" ,
+    [Piece.Black | Piece.King  ]: "black_king.svg"  ,
+    [Piece.Black | Piece.Pawn  ]: "black_pawn.svg"  ,
     [Piece.Black | Piece.Knight]: "black_knight.svg",
     [Piece.Black | Piece.Bishop]: "black_bishop.svg",
-    [Piece.Black | Piece.Rook]: "black_rook.svg",
-    [Piece.Black | Piece.Queen]: "black_queen.svg",
+    [Piece.Black | Piece.Rook  ]: "black_rook.svg"  ,
+    [Piece.Black | Piece.Queen ]: "black_queen.svg" ,
 };
