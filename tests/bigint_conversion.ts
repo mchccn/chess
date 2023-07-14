b0f7d509c18c3f99b412c11efd2efe6037544b60
kelsny <cursors.owo.uwu@gmail.com>
2023-07-14 14:28:58 -0500

    remove unused function

--- tests/bigint_conversion.ts
+++ tests/bigint_conversion.ts
@@ -3,8 +3,6 @@ const iterations = 10_000_000;
 const bigints = Array.from({ length: iterations }, () => BigInt(Math.floor((Math.random() * Number.MAX_SAFE_INTEGER))));
 const numbers = Array.from({ length: iterations }, () => Math.floor((Math.random() * Number.MAX_SAFE_INTEGER)));
 
-const reverse = (u64: bigint) => BigInt(u64.toString(2).split("").reverse().join(""));
-
 let aTime = 0n;
 let bTime = 0n;
 
@@ -32,3 +30,5 @@ for (const number of numbers) {
 
 console.log(`bigint to number: ${iterations} bigints converted in ${aTime / 1_000_000n}ms (~${Number(BigInt(iterations) / (aTime / 1_000_000n)) * 1000} per sec)`);
 console.log(`number to bigint: ${iterations} numbers converted in ${bTime / 1_000_000n}ms (~${Number(BigInt(iterations) / (bTime / 1_000_000n)) * 1000} per sec)`);
+
+export { };
