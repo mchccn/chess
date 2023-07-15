a6f158691f273d518af2c24c5fd1f5bf2c71671c
kelsny <cursors.owo.uwu@gmail.com>
2023-07-14 22:56:24 -0500

    revamp versus script

--- tests/versus.ts
+++ tests/versus.ts
@@ -1,45 +1,55 @@
-import { writeFileSync } from "fs";
+import { appendFileSync, readFileSync, readdirSync } from "fs";
 import { Adversary, Board, GameState, Piece } from "../src/index.js";
 
-const engineNamePlayingAsWhite = process.argv[2];
-const engineNamePlayingAsBlack = process.argv[3];
+const alreadyPlayed = readFileSync("tests/versus.txt", "utf8").split("\n").map((match) => match.split(":")[0]);
 
-if (!engineNamePlayingAsWhite || !engineNamePlayingAsBlack) {
-    console.log("missing engine name");
+const engines = await Promise.all(
+    readdirSync("src/adversaries", "utf8")
+        .filter((name) => name !== "Adversary.ts")
+        .map((name) => import(`../src/adversaries/${name}`)
+            .then((mod) => mod[name.slice(0, -".ts".length)])
+        )
+);
 
-    process.exit(1);
-}
+const toAppend: string[] = [];
 
-const enginePlayingAsWhite = (await import(`../dist/adversaries/${engineNamePlayingAsWhite}.js`))[engineNamePlayingAsWhite];
-const enginePlayingAsBlack = (await import(`../dist/adversaries/${engineNamePlayingAsBlack}.js`))[engineNamePlayingAsBlack];
+for (const white of engines) {
+    for (const black of engines) {
+        if (alreadyPlayed.includes(`${white.name} vs ${black.name}`)) continue;
 
-const playerWhite = new enginePlayingAsWhite() as Adversary;
-const playerBlack = new enginePlayingAsBlack() as Adversary;
+        const playerWhite = new white() as Adversary;
+        const playerBlack = new black() as Adversary;
 
-const gameResults: number[] = [];
+        const gameResults: number[] = [];
 
-for (let gameNumber = 0; gameNumber < 1000; gameNumber++) {
-    console.log(`playing game: #${gameNumber + 1}`);
+        for (let gameNumber = 0; gameNumber < 1000; gameNumber++) {
+            console.log(`playing game: #${gameNumber + 1}`);
 
-    const board = new Board().loadStartingPosition();
+            const board = new Board().loadStartingPosition();
 
-    while (board.gameState() === GameState.Playing) {
-        board.makeMove(
-            board.colorToMove === Piece.White
-                ? playerWhite.bestMove(board)
-                : playerBlack.bestMove(board)
-        );
+            while (board.gameState() === GameState.Playing) {
+                board.makeMove(
+                    board.colorToMove === Piece.White
+                        ? playerWhite.bestMove(board)
+                        : playerBlack.bestMove(board)
+                );
+            }
+
+            const state = board.gameState();
+
+            gameResults.push(state);
+
+            console.log(`${white.name} vs ${black.name} result: ${GameState.getName(state)}`);
+        }
+
+        toAppend.push(`${white.name} vs ${black.name}: ${
+            gameResults.filter((x) => x === GameState.WhiteCheckmatedBlack).length
+        } wins | ${
+            gameResults.filter((x) => GameState.isDraw(x)).length
+        } draws | ${
+            gameResults.filter((x) => x === GameState.BlackCheckmatedWhite).length
+        } losses`);
     }
-
-    const state = board.gameState();
-
-    gameResults.push(state);
-
-    console.log(`result: ${GameState.getName(state)}`);
 }
 
-writeFileSync(`${engineNamePlayingAsWhite}_vs_${engineNamePlayingAsBlack}.txt`, `\
-${gameResults.filter((x) => x === GameState.WhiteCheckmatedBlack).length} wins | ${gameResults.filter((x) => GameState.isDraw(x)).length} draws | ${gameResults.filter((x) => x === GameState.BlackCheckmatedWhite).length} losses
-game states: ${gameResults.join(", ")}
-state names: ${gameResults.map((x) => GameState.getName(x)).join(", ")}
-`, "utf8");
+appendFileSync("tests/versus.txt", toAppend.join("\n"), "utf8");
